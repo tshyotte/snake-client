6f6336dfe5062eb9481f329d232ed18d3ee1ff53
Odette Bahome <odettebahome@gmail.com>
2020-12-03 14:02:27 +0000

    add input module

--- play.js
+++ play.js
@@ -1,3 +1,12 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input');
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+
 console.log('Connecting ...');
 connect();
+setupInput();
